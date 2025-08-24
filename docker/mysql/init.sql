c93f74aa00a5999cb819c568f99571eb27bdb61c
Franco Callero <fcallero@frba.utn.edu.ar>
2025-08-24 16:43:57 -0300

    feat: dockerize project

---
+++ docker/mysql/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS `ms-compras` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `ms-productos-base` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `ms-productos-personalizados` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
